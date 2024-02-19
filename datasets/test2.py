8754f368a00e0247b5cee46b9d4d265f05a7364f
Julio Lopez <58756482+donlopez@users.noreply.github.com>
2024-02-19 00:57:35 +0000

    updtae to orlando2

--- datasets/test2.py
+++ datasets/test2.py
@@ -1,13 +1,10 @@
 import pandas as pd
 
 # Specify the file path
-file_path = "/workspaces/zillow-rent-vs-buy/datasets/orlando_fl_data.csv"
+file_path = "/workspaces/zillow-rent-vs-buy/datasets/orlando2_fl_data.csv"
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv(file_path)
 
-# Select all rows and the first 6 columns
-df_six_columns = df.iloc[:, :6]
-
-# Display the selected data
-print(df_six_columns)
+# Display the DataFrame
+print(df)
